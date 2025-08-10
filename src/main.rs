b60e4cbc762ea527ca29b22cc703ca171fdb6fc4
Dejun Wan <wandejun666@gmail.com>
2025-08-10 16:30:28 +0800

    feat(cli_parser): Add clap crate to parse command line arguments. Add lazy_static to hold cli_args and _logger_handle.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(incomplete_features)]
 #![feature(generic_const_exprs)]
+#![cfg_attr(debug_assertions, allow(dead_code))]
 
 mod config;
 mod cpu;
@@ -12,15 +13,40 @@ mod isa;
 mod logging;
 mod utils;
 
+use clap::Parser;
 pub use config::ram_config;
+use flexi_logger::LoggerHandle;
+use lazy_static::lazy_static;
 
-fn init() {
+lazy_static! {
+    static ref _logger_handle: LoggerHandle = logging::init();
+    static ref cli_args: Args = Args::parse();
+}
 
+fn init() {}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path of the target executable file(.elf/.bin)
+    path: std::path::PathBuf,
+
+    /// enable debug
+    #[arg(short = 'g', long = "debug", default_value_t = false)]
+    debug: bool,
+
+    /// Enable to print more details
+    #[arg(short, long, default_value_t = false)]
+    verbose: bool,
 }
 
 fn main() {
-    let _logger_handle = logging::init();
     init();
+
+    println!(
+        "path = {:?}, debug = {}, verbose = {}.",
+        cli_args.path, cli_args.debug, cli_args.verbose
+    );
 
     const A: [&'static str; 12] = gen_reg_name_list!("a", 1, 5; "b", 6, 10; "c"; "d");
     for i in 0..12 {
