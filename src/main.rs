4bff358eb85ddd46190c6b6e869ecd6d561edefc
satori_march <jiangdu9@outlook.com>
2025-08-11 20:42:15 +0800

    fix(logger): make logger work again

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,15 @@ mod utils;
 
 use clap::Parser;
 pub use config::ram_config;
-use flexi_logger::LoggerHandle;
 use lazy_static::lazy_static;
 
-use crate::{device::peripheral_init, handle_trait::HandleTrait};
+use crate::{
+    device::peripheral_init, handle_trait::HandleTrait, isa::riscv32, ram::Ram,
+    vaddr::VirtAddrManager,
+};
 
 lazy_static! {
     static ref cli_args: Args = Args::parse();
-    static ref _logger_handle: LoggerHandle = logging::init();
 }
 
 fn init() -> Vec<Box<dyn HandleTrait>> {
@@ -52,15 +53,7 @@ struct Args {
 }
 
 fn main() {
-    const A: [&'static str; 12] = gen_reg_name_list!("a", 1, 5; "b", 6, 10; "c"; "d");
-    for i in 0..12 {
-        if i == 11 {
-            println!("{}", A[i]);
-        } else {
-            print!("{}, ", A[i]);
-        }
-    }
-
+    let _logger_handle = logging::init();
     let _init_handle = init();
 
     println!(
