4d16cef2d63ac8543851598f9faf695c57241431
Dejun Wan <wandejun666@gmail.com>
2025-11-03 21:59:06 +0800

    feat(poller): add get_poller_result()

--- src/async_poller.rs
+++ src/async_poller.rs
@@ -3,6 +3,8 @@ use std::{
     thread,
 };
 
+use crossbeam::channel::{self, Receiver, Sender};
+
 use crate::device::fast_uart::virtual_io::TerminalIO;
 
 pub trait PollingEventTrait {
@@ -18,21 +20,31 @@ pub enum PollingEvent {
 
 pub struct AsyncPoller {
     enents: Arc<Mutex<Vec<PollingEvent>>>,
+    sender: Sender<InterryptID>,
+    receiver: Receiver<InterryptID>,
 }
 
 impl AsyncPoller {
     pub fn new() -> Self {
+        let (sender, receiver) = channel::unbounded();
         Self {
             enents: Arc::new(Mutex::new(vec![])),
+            sender,
+            receiver,
         }
     }
 
+    /// Register an epoll event. This event must implement the PollingEventTrait.
+    /// If an external interrupt needs to be triggered, the interrupt number can be
+    /// returned through the poll() function.
     pub fn add_event(&mut self, event: PollingEvent) {
         self.enents.lock().unwrap().push(event);
     }
 
+    /// Start a polling thread.
     pub fn start_polling(self) -> Self {
         let events = self.enents.clone();
+        let sender = self.sender.clone();
         thread::spawn(move || {
             loop {
                 // just scheduling enents by order.
@@ -40,7 +52,9 @@ impl AsyncPoller {
                 for event in guard.iter() {
                     match event {
                         PollingEvent::Uart(event) => {
-                            event.poll();
+                            if let Some(id) = event.poll() {
+                                sender.send(id).unwrap();
+                            }
                         }
                         PollingEvent::Stop => {
                             return; // exit current thread.
@@ -51,5 +65,15 @@ impl AsyncPoller {
             }
         });
         self
+    }
+
+    /// get the result of enent.poll().
+    pub fn get_poller_results(&self) -> Vec<InterryptID> {
+        let mut interrupts = vec![];
+        while let Ok(id) = self.receiver.recv() {
+            interrupts.push(id);
+        }
+
+        interrupts
     }
 }
