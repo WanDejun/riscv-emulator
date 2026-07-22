f3334d3e6a2666888c0686525609e6ef5c0fe8ed
Dejun Wan <wandejun666@gmail.com>
2026-07-22 21:59:00 +0800

    chore(dts): add sample virtio-mmio device tree node

--- src/device/plic/mod.rs
+++ src/device/plic/mod.rs
@@ -433,7 +433,6 @@ impl PLIC {
             return;
         }
         self.layout.set_pending(interrupt_id);
-        // self.update_all_context_irq_lines();
     }
 
     /// Clear a pending external interrupt source.
@@ -442,7 +441,6 @@ impl PLIC {
             return;
         }
         self.layout.clear_pending(interrupt_id);
-        // self.update_all_context_irq_lines();
     }
 
     /// Refresh the target CPU interrupt line for one PLIC context.
